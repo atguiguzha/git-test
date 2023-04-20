c84bd4d20e9e6ef0f155eb57d6fd8dc86dec697d
Jay <764672856@qq.com>
2023-04-20 14:43:33 +0800

    first commit

---
+++ src/main/java/package1/GitTest.java
@@ -0,0 +1,13 @@
+package package1;
+
+/**
+ * @author shkstart
+ * @description
+ * @e-mail 764672856@qq.com
+ * @create 2023-04-20 下午 2:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
