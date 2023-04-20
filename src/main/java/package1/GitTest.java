e92ee3e99968730f8e944ac3242576e851be6329
atguiguzha <131318742+atguiguzha@users.noreply.github.com>
2023-04-20 15:45:44 +0800

    Update GitTest.java

--- src/main/java/package1/GitTest.java
+++ src/main/java/package1/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix");
         System.out.println("push hot-fix");
+        System.out.println("pull hot-fix");
     }
 }
